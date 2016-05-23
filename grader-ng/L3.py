d31e04aa186001cb0213e41e8dd06500fb2a4ad2
Francisco Moya <francisco.moya@uclm.es>
2016-05-23 11:40:26 +0200

    Completed tests

--- grader-ng/L3.py
+++ grader-ng/L3.py
@@ -1,10 +1,10 @@
 # coding: utf-8
 
-def pau_header(cell):
-    return cell[0] == 'TOTAL'
+def pau_header(cells):
+    return cells[0] == 'TOTAL'
 
-def pau_footer(cell):
-    return cell[0] == ''
+def pau_footer(cells):
+    return cells[0] == ''
 
 def csv_extractor(path, row_extractor,
                   header_condition = pau_header,
@@ -13,39 +13,74 @@ def csv_extractor(path, row_extractor,
     with open(path,'r') as f:
         header = True
         for row in f:
-            cell = row.strip().split(',')
+            cells = row.strip().split(',')
             if not header:
-                if footer_condition(cell):
+                if footer_condition(cells):
                     break
-                ret.append(row_extractor(cell))
-            elif header_condition(cell):
+                ret.append(row_extractor(cells))
+            elif header_condition(cells):
                 header = False
     return ret
 
-
-def get_column_0(cell):
-    return cell[0]
+def get_column_0(cells):
+    return cells[0]
 
 def comunidades_autonomas(path):
     ret = csv_extractor(path, get_column_0)
     return sorted(ret)
 
-def get_column_0_
+def get_columns_0_11(cells):
+    return (cells[0],value_as_int(cells[11]))
+
+def value_as_int(v):
+    return int(value_as_float(v))
+
+def value_as_float(v):
+    if v[0] == '"':
+        v = v[1:-1]
+    return float(v)
 
 def aprobados_pau(path):
-    return { d[0]:d[1] for d in csv_extractor(path, get_columns_0_x) }
+    return { d[0]:d[1] for d in csv_extractor(path, get_columns_0_11) }
 
 
-def suspensos_pau():
-    pass
+def get_columns_0_1_minus_11(cells):
+    return (cells[0],value_as_int(cells[1]) - value_as_int(cells[11]))
 
 
-def comunidad_mas_aprobados():
-    pass
+def suspensos_pau(path, comunidad):
+    susp = { d[0]:d[1] for d in csv_extractor(path, get_columns_0_1_minus_11) }
+    if comunidad in susp:
+        return susp[comunidad]
+    raise ValueError('No hay datos de ' + comunidad)
+
+def get_columns_0_11_div_into_1(cells):
+    return (cells[0],value_as_float(cells[11])/value_as_float(cells[1]))
+
+def column_1(cells):
+    return cells[1]
+
+def comunidad_mas_aprobados(path):
+    return sorted(csv_extractor(path, get_columns_0_11_div_into_1),
+                  key = column_1, reverse=True)[0][0]
+
+def column_1(cells):
+    return cells[1]
+
+def get_diff_rate(cells):
+    jun = value_as_float(cells[13])/value_as_float(cells[3])
+    sep = value_as_float(cells[15])/value_as_float(cells[5])
+    return (cells[0], abs(jun-sep))
+    
+def comunidad_mas_diferencia(path):
+    return sorted(csv_extractor(path, get_diff_rate),
+                  key = column_1, reverse=True)[0]
 
 
-def comunidad_mas_diferencia():
-    pass
-
-
-print comunidades_autonomas('tests/pcaxis637310428.csv')
+if __name__ == '__main__':
+    print comunidades_autonomas('tests/pcaxis637310428.csv')
+    print aprobados_pau('tests/pcaxis637310428.csv')
+    print suspensos_pau('tests/pcaxis637310428.csv', 'Extremadura')
+    print suspensos_pau('tests/pcaxis637310428.csv', 'Canarias')
+    print comunidad_mas_aprobados('tests/pcaxis637310428.csv')
+    print comunidad_mas_diferencia('tests/pcaxis637310428.csv')
--- grader-ng/tests/T3.py
+++ grader-ng/tests/T3.py
@@ -12,20 +12,29 @@ csv = 'tests/pcaxis637310428.csv'
 class Test_3(unittest.TestCase):
     
     def test_comunidades_autonomas(self):
-        self.assertEqual([], comunidades_autonomas(csv))
-
+        ca = sorted(comunidades_autonomas(csv))
+        self.assertEqual(ca[0], 'A distancia')
+        self.assertEqual(ca[3], 'Asturias; Principado de')
+        self.assertEqual(ca[4], 'Balears; Illes')
+        self.assertEqual(ca[5], 'Canarias')
+        self.assertEqual(ca[17], 'Rioja; La')
     
     def test_aprobados_pau(self):
-        pass
-
+        aprobados = aprobados_pau(csv)
+        self.assertEqual(10103, aprobados['Canarias'])
+        self.assertEqual(10497, aprobados['Galicia'])
+        self.assertEqual(8648, aprobados['Castilla - La Mancha'])
+        self.assertEqual(23536, aprobados['Comunitat Valenciana'])
     
     def test_suspensos_pau(self):
-        pass
+        self.assertEqual(736, suspensos_pau('tests/pcaxis637310428.csv',
+                                            'Extremadura'))
+        self.assertEqual(1061, suspensos_pau('tests/pcaxis637310428.csv',
+                                             'Canarias'))
 
-    
     def test_comunidad_mas_aprobados(self):
-        pass
-
+        self.assertEqual('Rioja; La',
+                         comunidad_mas_aprobados('tests/pcaxis637310428.csv'))
     
     def test_comunidad_mas_diferencia(self):
         pass
