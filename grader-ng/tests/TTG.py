fefbd76dce36710755e27cb4369feebae5ae1c23
Francisco Moya <francisco.moya@uclm.es>
2016-06-16 22:36:52 +0200

    Improved manual evaluation. Per item comments.

--- grader-ng/tests/TTG.py
+++ grader-ng/tests/TTG.py
@@ -1,38 +1,30 @@
 # encoding: utf-8
+from __future__ import print_function
 import unittest, sys
 from .manual import *
 
 class Test_TG(unittest.TestCase):
 
     def test_manual(self):
-        agregar('TOTAL',
-                [self.correccion_funcional(),
-                 self.tratamiento_excepciones(),
-                 self.estructura(),
-                 self.pruebas()])
-        print_output()
+        m = ManualGrading('TRABAJO EN GRUPO')
+        m.add('TOTAL',
+              [m.add('Corrección funcional',
+                     [m.new('Descarga de archivos', 2.5),
+                      m.new('Procesamiento XML', 2.5),
+                      m.new('Generación de gráficas', 2.5)]),
+               m.add('Tratamiento de excepciones',
+                     [m.new('Errores de comunicación', .5),
+                      m.new('Errores en datos y archivos', .5)]),
+               m.add('Estructura del programa',
+                     [m.new('Divide y vencerás', .5),
+                      m.new('Diseño top-down', .5),
+                      m.new('Código limpio', .5)]),
+               m.add('Puntos extra',
+                     [m.new('Banco de pruebas', 1.)])])
+        m.print_output()
+        print('\nMáxima calificación posible: {}'.
+              format(round(m.total,2)),
+              file=sys.stderr)
 
-    def correccion_funcional(self):
-        return agregar('Corrección funcional',
-                       [manual('Descarga de archivos', .25),
-                        manual('Procesamiento XML', .25),
-                        manual('Generación de gráficas', .25)])
-
-    def tratamiento_excepciones(self):
-        return agregar('Tratamiento de excepciones',
-                       [manual('Errores de comunicación', .05),
-                        manual('Errores en datos y archivos', .05)])
-
-    def estructura(self):
-        return agregar('Estructura del programa',
-                       [manual('Divide y vencerás', .05),
-                        manual('Diseño top-down', .05),
-                        manual('Código limpio', .05)])
-        
-    def pruebas(self):
-        return agregar('Puntos extra',
-                       [manual('Banco de pruebas', .10)])
-
-        
 if __name__ == '__main__':
     Test_TG().test_manual()
--- grader-ng/tests/manual.py
+++ grader-ng/tests/manual.py
@@ -1,34 +1,46 @@
+# encoding: utf-8
 from __future__ import print_function
 from builtins import input
 import sys
 
-__ttg_output=''
+class ManualGrading(object):
+    def __init__(self, title):
+        self._items=title
+        self._comment=''
+        self._total=0.
 
-def input_grade(item, points):
-    while True:
-        print(item + ': ', end='')
-        try:
-            grade = float(input())
-        except EOFError:
-            raise
-        except:
-            continue
-        if grade <= points:
-            return grade
-        print('Debe ser <= {}'.format(points))
+    def input_grade(self, item, points):
+        while True:
+            # El funcionamiento de input(prompt) es muy extraño:
+            # - Si stderr se redirige el prompt va a stderr
+            # - En caso contrario el prompt va a stdout
+            try:
+                print('{} ({}): '.format(item, points), end='')
+                grade = float(input())
+            except EOFError: raise
+            except:          continue
+            print('Comentario para {}: '.format(item))
+            self._comment += input()
+            if grade <= points:
+                return grade
+            print('Debe ser <= {}'.format(points))
 
-def manual(item, points):
-    global __ttg_output
-    grade = input_grade(item, points)
-    __ttg_output += '\n  {} {}'.format(grade, item)
-    return grade
+    def new(self, item, points):
+        grade = self.input_grade(item, points)
+        self._items += '\n  {} {}'.format(grade, item)
+        self._total += points
+        return grade
 
-def agregar(item, list):
-    global __ttg_output
-    grade = round(sum(list),2)
-    __ttg_output += '\n{} {}'.format(grade, item)
-    return grade
+    def add(self, item, list):
+        grade = round(sum(list),2)
+        self._items += '\n{} {}'.format(grade, item)
+        return grade
 
-def print_output():
-    global __ttg_output
-    print(__ttg_output, file=sys.stderr)
+    def print_output(self):
+        print(self._items, file=sys.stderr)
+        print('-'*70, file=sys.stderr)
+        print(self._comment, file=sys.stderr)
+
+    @property
+    def total(self):
+        return self._total
