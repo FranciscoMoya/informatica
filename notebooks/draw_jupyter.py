41a9b2256cf047394c8cf3b8c31a6e3083cae3c7
Francisco Moya <francisco.moya@uclm.es>
2016-11-22 22:24:31 +0100

    fixed inconsistency in jupyter draw module

--- notebooks/draw_jupyter.py
+++ notebooks/draw_jupyter.py
@@ -40,7 +40,7 @@ def limpiar():
     current_drawing = ''
     comenzar()
 
-def screen():
+def dibujo():
     global current_drawing
     ret = SVG('''<svg width="400" height="400" viewBox="0 0 400 400"
                       xmlns="http://www.w3.org/2000/svg">{}</svg>'''\
